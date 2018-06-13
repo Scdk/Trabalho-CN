67c6991f4282cd7d27b630471e67f8e6c85ef4be
Lucas Resende Carneiro <lucasrescanrneiro@gmail.com>
2018-06-13 18:19:25 -0300

    Adicionei a relação função.

--- main.c
+++ main.c
@@ -145,6 +145,29 @@ void irreflexiva(int bin, FILE *arq){
       fprintf(arq, "%s", " I ");
 }
 
+void funcao(int bin, FILE *arq){
+    int i, j, x, flag = 0, flag2 = 0;
+    for(i = 1; i < 5; i++){
+        for(j = 1; j < 5; j++){
+            if(aRb(i, j, bin) == 1){
+               flag = 1;
+               if(x == 1){
+                    flag = 0;
+                    break;
+               }
+                x = 1;
+            }
+        }
+        x = 0;
+        if(flag == 1){
+            flag2++;
+            flag = 0;
+        }
+    }
+    if(flag2 == 4)
+        fprintf(arq, "%s", " F ");
+}
+
 int main(){
 	int bin = 0, x = 0, s = 0, t = 0, r = 0;
 	FILE *arq;
@@ -156,6 +179,7 @@ int main(){
         r = reflexiva(bin, arq);
         equivalencia(s, t, r, arq);
         irreflexiva(bin, arq);
+        funcao(bin, arq);
         fprintf(arq, "%s", "\n");
         x = bin & 65535;
         bin++;
